ef3e90bc7aeceaab351bce88a1647a4e42f9c8a2
G1of1 <ngsuna24@gmail.com>
2025-09-07 20:53:12 -0400

    Auth: Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
-import NextAuthProvider from "@/providers/SessionProvider";
-
+import { SessionProvider as Provider } from "@/providers/SessionProvider";
 export const metadata = {
   title: "Syllabus Calendar",
   description: "Turn your syllabus into a calendar with LawBandit",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  
   return (
     <html lang="en">
       <body>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
