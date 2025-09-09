f10004d6536e4d1a6fc87d15264cda530b7e59a9
G1of1 <ngsuna24@gmail.com>
2025-09-09 17:23:59 -0400

    Update: layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 import { SessionProvider as Provider } from "@/providers/SessionProvider";
+import { Toaster } from "../components/ui/sonner";
 export const metadata = {
-  title: "Syllabus Calendar",
+  title: "SyllaSync",
   description: "Turn your syllabus into a calendar with LawBandit",
+  icons: "icons/lawbandit.svg"
 };
 
 export default async function RootLayout({
@@ -16,6 +18,7 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <Provider>{children}</Provider>
+        <Toaster position="top-right" />
       </body>
     </html>
   );
